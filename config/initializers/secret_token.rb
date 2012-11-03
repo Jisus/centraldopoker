ab691f641b11f7188ca2462f4ec1b0ee8f2106ed
Rodrigo Dias <rodrigo.dias@planb.com.br>
2012-11-03 14:43:48 -0200

    commitInit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Centralpoker::Application.config.secret_token = '9e73c03e0ebcfeabd277563ece65cf24d9ddfcf8108a93847a45e4ad1df5d60be08e75190a5225a68954710ef485468b0ae361f66964709813a721320c09ccbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Centralpoker::Application.config.session_store :cookie_store, key: '_centralpoker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Centralpoker::Application.config.session_store :active_record_store
